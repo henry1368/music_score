55ca1dda59bf6e65d1ad1f587de17389f83edad2
lyh <621821858@qq.com>
2025-02-24 20:59:38 +0800

    multi notes (use midi)

--- mp4.py
+++
@@ -1,9 +0,0 @@
-from pydub import  AudioSegment
-
-mp4file = "test.mp4"
-mp3file = "output.mp3"
-
-audio = AudioSegment.from_file(mp4file, format="mp4")
-audio.export(mp3file, format="mp3")
-
-print(f"output{mp3file}")
